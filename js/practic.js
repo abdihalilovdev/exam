2416e55ca513ff571cc680d3d63934a4afc20e90
abdihalilovdev <zabdihalilov23@gmail.com>
2023-01-29 11:18:41 +0600

    add-change

--- js/practic.js
+++ js/practic.js
@@ -1,89 +1,89 @@
-const firstInput = document.querySelector('.first-input')
-const secondInput = document.querySelector('.second-input')
-const btn = document.querySelector('.add-btn')
-const ul = document.querySelector('.ul')
-// localStorage.setItem('task', JSON.stringify([{name: 'Javhar', username: 'Abdihalilov'}, {
-//     name: 'Elmar',
-//     username: 'Abdihalilov'
-// }]))
-function view() {
-    const tasks = JSON.parse(localStorage.getItem('task'))
-    ul.innerHTML = ''
-    tasks.map((el, idx) => {
-        ul.innerHTML += `<li class="list-group-item d-flex justify-content-between align-items-center">
-        <div class="line">${el.name[0].toUpperCase()}${el.username[0].toUpperCase()}</div>  Name: ${el.name} Username:  ${el.username}
-        <button class="del-btn btn btn-danger">delete</button></li>`
-    })
-    deleteTask()
-    firstInput.value = ''
-    secondInput.value = ''
-}
-view()
-btn.addEventListener('click', () => {
-   if (firstInput.value !== '' && secondInput.value !== ''){
-       let tasks = JSON.parse(localStorage.getItem('task'))
-       const newTask = {
-           id:1,
-           name:firstInput.value[0].toUpperCase() + firstInput.value.slice(1),
-           username:secondInput.value[0].toUpperCase() + secondInput.value.slice(1),
-       }
-       tasks = [...tasks,newTask]
-       localStorage.setItem('task',JSON.stringify(tasks))
-       view()
-       firstInput.style.border = '1px solid green'
-       secondInput.style.border = '1px solid green'
-   } else if (firstInput.value === '' && secondInput.value === ''){
-       firstInput.style.border = '3px solid red'
-       secondInput.style.border = '3px solid red'
-   }
-})
-function deleteTask(){
-    let tasks = JSON.parse(localStorage.getItem('task'))
-    const  buttons = document.querySelectorAll('.del-btn')
-    buttons.forEach((btn,index)=>{
-        btn.addEventListener('click',()=>{
-            tasks = tasks.filter((el,idx)=>{
-                return index !== idx
-            })
-            localStorage.setItem('task',JSON.stringify(tasks))
-            view()
-        })
-    })
-}
-firstInput.addEventListener('keydown',(e)=>{
-    if (e.key === 'Enter'){
-        secondInput.focus()
-    }
-})
-secondInput.addEventListener('keydown',(e)=>{
-    if (e.key === 'Enter'){
-        btn.focus()
-    }
-})
-btn.addEventListener('keydown',(e)=>{
-    if (e.key === 'Enter'){
-        firstInput.focus()
-    }
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// const firstInput = document.querySelector('.first-input')
+// const secondInput = document.querySelector('.second-input')
+// const btn = document.querySelector('.add-btn')
+// const ul = document.querySelector('.ul')
+// // localStorage.setItem('task', JSON.stringify([{name: 'Javhar', username: 'Abdihalilov'}, {
+// //     name: 'Elmar',
+// //     username: 'Abdihalilov'
+// // }]))
+// function view() {
+//     const tasks = JSON.parse(localStorage.getItem('task'))
+//     ul.innerHTML = ''
+//     tasks.map((el, idx) => {
+//         ul.innerHTML += `<li class="list-group-item d-flex justify-content-between align-items-center">
+//         <div class="line">${el.name[0].toUpperCase()}${el.username[0].toUpperCase()}</div>  Name: ${el.name} Username:  ${el.username}
+//         <button class="del-btn btn btn-danger">delete</button></li>`
+//     })
+//     deleteTask()
+//     firstInput.value = ''
+//     secondInput.value = ''
+// }
+// view()
+// btn.addEventListener('click', () => {
+//    if (firstInput.value !== '' && secondInput.value !== ''){
+//        let tasks = JSON.parse(localStorage.getItem('task'))
+//        const newTask = {
+//            id:1,
+//            name:firstInput.value[0].toUpperCase() + firstInput.value.slice(1),
+//            username:secondInput.value[0].toUpperCase() + secondInput.value.slice(1),
+//        }
+//        tasks = [...tasks,newTask]
+//        localStorage.setItem('task',JSON.stringify(tasks))
+//        view()
+//        firstInput.style.border = '1px solid green'
+//        secondInput.style.border = '1px solid green'
+//    } else if (firstInput.value === '' && secondInput.value === ''){
+//        firstInput.style.border = '3px solid red'
+//        secondInput.style.border = '3px solid red'
+//    }
+// })
+// function deleteTask(){
+//     let tasks = JSON.parse(localStorage.getItem('task'))
+//     const  buttons = document.querySelectorAll('.del-btn')
+//     buttons.forEach((btn,index)=>{
+//         btn.addEventListener('click',()=>{
+//             tasks = tasks.filter((el,idx)=>{
+//                 return index !== idx
+//             })
+//             localStorage.setItem('task',JSON.stringify(tasks))
+//             view()
+//         })
+//     })
+// }
+// firstInput.addEventListener('keydown',(e)=>{
+//     if (e.key === 'Enter'){
+//         secondInput.focus()
+//     }
+// })
+// secondInput.addEventListener('keydown',(e)=>{
+//     if (e.key === 'Enter'){
+//         btn.focus()
+//     }
+// })
+// btn.addEventListener('keydown',(e)=>{
+//     if (e.key === 'Enter'){
+//         firstInput.focus()
+//     }
+// })
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
